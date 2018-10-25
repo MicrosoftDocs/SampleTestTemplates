500b704f6224adfb4e599cbedcd2f58ad6b1ee08
Mikhail Melnikov <v-mimeln@microsoft.com>
2018-10-25 16:38:28 +0300

    Debug

--- nodejs/check_node_js.sh
+++ nodejs/check_node_js.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 echo "Node.Js check started"
-pwd
-ls
 echo "Found files:"
 find -name '*.js'
 echo "Check results:"
-find -name '*.js' | echo
-set -o pipefail
 find -name '*.js' | xargs node --check
