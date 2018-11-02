175939ae999b6afee1ce3aad46f0140cabc0dc80
Mikhail Melnikov <v-mimeln@microsoft.com>
2018-11-02 18:43:40 +0400

    Fixed node.js and maven

--- nodejs/check_node_js.sh
+++ nodejs/check_node_js.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "Node.Js check started"
-echo "Found files:"
-find -name '*.js'
+found=$(find -name '*.js' | wc -l)
+if (( $found == 0)); then
+    echo "Js files not found!"
+	exit 1 
+fi
 echo "Check results:"
 find -name '*.js' | xargs -n1 node --check
